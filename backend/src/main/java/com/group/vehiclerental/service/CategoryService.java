7302f4381ef5779788b3dc1e87397b99762a2677
Agresara <soysaagresara@gmail.com>
2026-07-30 23:29:54 +0530

    Update CategoryService.java

--- backend/src/main/java/com/group/vehiclerental/service/CategoryService.java
+++ backend/src/main/java/com/group/vehiclerental/service/CategoryService.java
@@ -24,6 +24,7 @@ public class CategoryService {
                            VehicleRepository vehicleRepository) {
         this.categoryRepository = categoryRepository;
         this.vehicleRepository = vehicleRepository;
+        
     }
 
     @Transactional(readOnly = true)
